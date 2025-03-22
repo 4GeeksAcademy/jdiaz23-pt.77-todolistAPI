616f98df29aad76204455b6286c5bebb6b85ddf2
jdiaz1993 <diazjoseph366@gmail.com>
2025-03-22 00:42:41 +0000

    final?

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import { useonKeyPress } from "./Hooks/useOnKeyPress";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [tasks, setTasks ] = useState([]);
+	const [userInput, setUserInput] = useState("");
+	
+
+const addToDo = (e) => {
+	e.preventDefault ();
+	let task = {text: userInput, finished : false}
+	setTasks([...tasks, task]);
+	setUserInput("");
+}
+
+const removeTask = (i) => {
+	const newArray = tasks.filter((task, index) => index !== i);
+	setTasks(newArray)
+}
+
+const keyPressHandler = (event) => {
+   console.log(event.key)
+	if (event.key === "Enter") {
+        addToDo(event);
+    }
+   };
+
+	return (
+			
+		<div className="container text-center mt-5 h-100">
+			<h1>To Do List</h1>
+			
+			<input type="text" placeholder="What needs to be done?"
+			onChange={(e) => setUserInput(e.target.value)} 
+			onKeyDown={(event) =>{keyPressHandler(event)} }
+			value={userInput}
+			
+			/>
+            <button onClick={ (e) => addToDo(e)}>Update List</button>
+			<ul>
+				{tasks?.map((task,index) => {
+					if (task !=true) {
+						return (<li key={index}>{task.text}<span onClick={() =>removeTask(index)} id="redX"> ❌ </span> </li>)
+					}
+				})}
+			</ul>
+			
 		</div>
 	);
 };
